013392de8807e2a16ab4385e966e14d5d56e6b52
Jonathan Oliveira <jonathan@jonathanoliveira.org>
2017-07-26 15:46:15 -0400

    N-Bit Counter created. This will be used to access the RAM and control the processor.

---
+++ src/org/jonathanoliveira/complex_components/Counter_Nbit.java
@@ -0,0 +1,81 @@
+package org.jonathanoliveira.complex_components;
+
+import org.jonathanoliveira.logic_gates.MUX_Gate;
+import org.jonathanoliveira.logic_gates.Nbit_AND_Gate;
+import org.jonathanoliveira.logic_gates.Nbit_NOT_Gate;
+
+/**
+ * This class will be the counter for all accessing the RAM and controlling the ALU.
+ * It will have a 16bit number input and a 3bit function input that will control the outcome of this counter.
+ * L = load -> will load a particular number into the counter
+ * I = increment -> will increment the number stored inside the counter with 1
+ * R = reset -> will set the counter to 0
+ * L | I | R | res
+ * 1 | 0 | 0 | load the counter with in[16]
+ * 0 | 0 | 1 | reset the counter to 0000...
+ * 0 | 1 | 0 | increment the counter with 1 (if 1111... -> 0000...)
+ * 0 | 0 | 0 | nothing happens, the output of the counter maintains the same
+ * ** notice that the function inputs must be set to 1 only one at a time
+ */
+public class Counter_Nbit {
+
+    private int bitSize;
+    private boolean L;
+    private boolean I;
+    private boolean R;
+    private boolean[] input;
+    private boolean[] output;
+
+    private Nbit_NOT_Gate inverter;
+    private Nbit_AND_Gate andGate;
+
+    private Register_Nbit register;
+    private N_BitIncrementer incrementer;
+    private MUX_Gate incMux;
+
+    public Counter_Nbit(int bitSize) {
+        this.bitSize = bitSize;
+        this.output = new boolean[bitSize];
+        this.input = new boolean[bitSize];
+        this.register = new Register_Nbit(bitSize);
+        this.incrementer = new N_BitIncrementer(bitSize);
+        this.incMux = new MUX_Gate(bitSize);
+        this.inverter = new Nbit_NOT_Gate(bitSize);
+        this.andGate = new Nbit_AND_Gate(bitSize);
+    }
+
+    public boolean[] getOutput() {
+        wire();
+        return output;
+    }
+
+    public void setCounter(boolean[] input, boolean[] function) {
+        int testFunc = 0;
+        for (boolean bool : function) {
+            if (bool) testFunc++;
+        }
+        if (testFunc > 1 || input.length != bitSize || function.length != 3) throw new IllegalArgumentException();
+        this.input = input;
+        this.L = function[0];
+        this.I = function[1];
+        this.R = function[2];
+    }
+
+    private void wire() {
+
+        this.inverter.setInputs(this.input);
+        this.andGate.setInputs(this.input, this.inverter.getOutput());
+
+        this.register.write(this.R);
+        this.register.data(this.andGate.getOutput());
+
+        this.register.write(this.L);
+        this.register.data(this.input);
+
+        this.incrementer.setInput(this.register.Q());
+        this.incMux.setGate(this.register.Q(), this.incrementer.getOutput(), this.I);
+        this.register.write(this.I);
+        this.register.data(this.incMux.getOutput());
+        this.output = this.incMux.getOutput();
+    }
+}
--- tests/org/jonathanoliveira/complex_components/Counter16BitTest.java
+++ tests/org/jonathanoliveira/complex_components/Counter16BitTest.java
@@ -3,11 +3,13 @@ package org.jonathanoliveira.complex_components;
 import org.jonathanoliveira.utilities.Converter;
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
+
 public class Counter16BitTest {
 
-/*    @Test (expected = IllegalArgumentException.class)
+    @Test (expected = IllegalArgumentException.class)
     public void checkInvalidFunction() throws Exception {
-        Counter_16Bit counter = new Counter_16Bit(3);
+        Counter_Nbit counter = new Counter_Nbit(3);
         boolean[] func = Converter.convertToBooleans(new int[]{1,1,0});
         boolean[] input = Converter.convertToBooleans(new int[]{0,1,0});
         counter.setCounter(input, func);
@@ -15,17 +17,17 @@ public class Counter16BitTest {
 
     @Test (expected = IllegalArgumentException.class)
     public void checkInvalidInput() throws Exception {
-        Counter_16Bit counter = new Counter_16Bit(3);
+        Counter_Nbit counter = new Counter_Nbit(3);
         boolean[] func = Converter.convertToBooleans(new int[]{0,1,0});
         boolean[] input = Converter.convertToBooleans(new int[]{0,1});
         counter.setCounter(input, func);
     }
 
+    @Test
     public void checkIncrement() throws Exception {
-        Counter_16Bit counter = new Counter_16Bit(3);
+        Counter_Nbit counter = new Counter_Nbit(3);
         boolean[] func = Converter.convertToBooleans(new int[]{0,1,0});
-        boolean[] input = Converter.convertToBooleans(new int[]{0,1});
-        counter.setCounter(input, func);
+        boolean[] input = Converter.convertToBooleans(new int[]{0,1,0});
         boolean[] exp1 = Converter.convertToBooleans(new int[]{0,0,1});
         boolean[] exp2 = Converter.convertToBooleans(new int[]{0,1,0});
         boolean[] exp3 = Converter.convertToBooleans(new int[]{0,1,1});
@@ -34,6 +36,8 @@ public class Counter16BitTest {
         boolean[] exp6 = Converter.convertToBooleans(new int[]{1,1,0});
         boolean[] exp7 = Converter.convertToBooleans(new int[]{1,1,1});
         boolean[] exp8 = Converter.convertToBooleans(new int[]{0,0,0});
+        assertArrayEquals(exp8, counter.getOutput());
+        counter.setCounter(input, func);
         assertArrayEquals(exp1, counter.getOutput());
         assertArrayEquals(exp2, counter.getOutput());
         assertArrayEquals(exp3, counter.getOutput());
@@ -42,5 +46,105 @@ public class Counter16BitTest {
         assertArrayEquals(exp6, counter.getOutput());
         assertArrayEquals(exp7, counter.getOutput());
         assertArrayEquals(exp8, counter.getOutput());
-    }*/
+        assertArrayEquals(exp1, counter.getOutput());
+        assertArrayEquals(exp2, counter.getOutput());
+        assertArrayEquals(exp3, counter.getOutput());
+        func = Converter.convertToBooleans(new int[]{0,0,0});
+        counter.setCounter(input, func);
+        assertArrayEquals(exp3, counter.getOutput());
+        assertArrayEquals(exp3, counter.getOutput());
+        assertArrayEquals(exp3, counter.getOutput());
+        func = Converter.convertToBooleans(new int[]{0,1,0});
+        counter.setCounter(input, func);
+        assertArrayEquals(exp4, counter.getOutput());
+        assertArrayEquals(exp5, counter.getOutput());
+        assertArrayEquals(exp6, counter.getOutput());
+        assertArrayEquals(exp7, counter.getOutput());
+        assertArrayEquals(exp8, counter.getOutput());
+        assertArrayEquals(exp1, counter.getOutput());
+    }
+
+    @Test
+    public void checkLoad() throws Exception {
+        Counter_Nbit counter = new Counter_Nbit(3);
+        boolean[] func = Converter.convertToBooleans(new int[]{1,0,0});
+        boolean[] input = Converter.convertToBooleans(new int[]{1,1,0});
+        boolean[] exp1 = Converter.convertToBooleans(new int[]{0,0,1});
+        boolean[] exp2 = Converter.convertToBooleans(new int[]{0,1,0});
+        boolean[] exp3 = Converter.convertToBooleans(new int[]{0,1,1});
+        boolean[] exp4 = Converter.convertToBooleans(new int[]{1,0,0});
+        boolean[] exp5 = Converter.convertToBooleans(new int[]{1,0,1});
+        boolean[] exp6 = Converter.convertToBooleans(new int[]{1,1,0});
+        boolean[] exp7 = Converter.convertToBooleans(new int[]{1,1,1});
+        boolean[] exp8 = Converter.convertToBooleans(new int[]{0,0,0});
+        assertArrayEquals(exp8, counter.getOutput());
+        counter.setCounter(input, func);
+        assertArrayEquals(exp6, counter.getOutput());
+        assertArrayEquals(exp6, counter.getOutput());
+        func = Converter.convertToBooleans(new int[]{0,0,0});
+        counter.setCounter(input, func);
+        assertArrayEquals(exp6, counter.getOutput());
+        func = Converter.convertToBooleans(new int[]{0,1,0});
+        counter.setCounter(input, func);
+        assertArrayEquals(exp7, counter.getOutput());
+        assertArrayEquals(exp8, counter.getOutput());
+        assertArrayEquals(exp1, counter.getOutput());
+        func = Converter.convertToBooleans(new int[]{0,0,0});
+        counter.setCounter(input, func);
+        assertArrayEquals(exp1, counter.getOutput());
+        func = Converter.convertToBooleans(new int[]{1,0,0});
+        counter.setCounter(input, func);
+        assertArrayEquals(exp6, counter.getOutput());
+        func = Converter.convertToBooleans(new int[]{0,0,0});
+        counter.setCounter(input, func);
+        assertArrayEquals(exp6, counter.getOutput());
+        func = Converter.convertToBooleans(new int[]{0,1,0});
+        counter.setCounter(input, func);
+        assertArrayEquals(exp7, counter.getOutput());
+        assertArrayEquals(exp8, counter.getOutput());
+        assertArrayEquals(exp1, counter.getOutput());
+        assertArrayEquals(exp2, counter.getOutput());
+        assertArrayEquals(exp3, counter.getOutput());
+    }
+
+    @Test
+    public void checkReset() throws Exception {
+        Counter_Nbit counter = new Counter_Nbit(3);
+        boolean[] load = Converter.convertToBooleans(new int[]{1,0,0});
+        boolean[] inc = Converter.convertToBooleans(new int[]{0,1,0});
+        boolean[] reset = Converter.convertToBooleans(new int[]{0,0,1});
+        boolean[] stall = Converter.convertToBooleans(new int[]{0,0,0});
+        boolean[] input = Converter.convertToBooleans(new int[]{1,1,0});
+        boolean[] exp1 = Converter.convertToBooleans(new int[]{0,0,1});
+        boolean[] exp2 = Converter.convertToBooleans(new int[]{0,1,0});
+        boolean[] exp3 = Converter.convertToBooleans(new int[]{0,1,1});
+        boolean[] exp4 = Converter.convertToBooleans(new int[]{1,0,0});
+        boolean[] exp5 = Converter.convertToBooleans(new int[]{1,0,1});
+        boolean[] exp6 = Converter.convertToBooleans(new int[]{1,1,0});
+        boolean[] exp7 = Converter.convertToBooleans(new int[]{1,1,1});
+        boolean[] exp8 = Converter.convertToBooleans(new int[]{0,0,0});
+        assertArrayEquals(exp8, counter.getOutput());
+        counter.setCounter(input, load);
+        assertArrayEquals(exp6, counter.getOutput());
+        assertArrayEquals(exp6, counter.getOutput());
+        counter.setCounter(input, reset);
+        assertArrayEquals(exp8, counter.getOutput());
+        counter.setCounter(input, inc);
+        assertArrayEquals(exp1, counter.getOutput());
+        assertArrayEquals(exp2, counter.getOutput());
+        assertArrayEquals(exp3, counter.getOutput());
+        counter.setCounter(input, stall);
+        assertArrayEquals(exp3, counter.getOutput());
+        counter.setCounter(input, load);
+        assertArrayEquals(exp6, counter.getOutput());
+        counter.setCounter(input, inc);
+        assertArrayEquals(exp7, counter.getOutput());
+        counter.setCounter(input, reset);
+        assertArrayEquals(exp8, counter.getOutput());
+        counter.setCounter(input, inc);
+        assertArrayEquals(exp1, counter.getOutput());
+        assertArrayEquals(exp2, counter.getOutput());
+        counter.setCounter(input, stall);
+        assertArrayEquals(exp2, counter.getOutput());
+    }
 }
