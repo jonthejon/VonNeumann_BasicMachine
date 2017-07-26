299fe7f3da102fad78f95945bbea961b2c605332
Jonathan Oliveira <jonathan@jonathanoliveira.org>
2017-07-26 17:11:34 -0400

    Created RAM8X1 chip.

---
+++ src/org/jonathanoliveira/complex_components/RAM_8x1.java
@@ -0,0 +1,65 @@
+package org.jonathanoliveira.complex_components;
+
+import org.jonathanoliveira.logic_gates.DMUX_8Way_Gate;
+import org.jonathanoliveira.logic_gates.MUX_8Way_Gate;
+
+/**
+ * Ram chip.
+ * This chip stores 8 different 1bit values
+ */
+
+public class RAM_8x1 {
+
+    private int bitSize;
+    private int numRegisters;
+    private boolean dataIn;
+    private boolean dataOut;
+    private boolean write;
+    private boolean[] address;
+
+    private Register_1Bit[] registers;
+    private DMUX_8Way_Gate dmux;
+    private MUX_8Way_Gate mux;
+
+    public RAM_8x1() {
+        this.bitSize = 1;
+        this.numRegisters = 8;
+        this.address = new boolean[3];
+        this.registers = new Register_1Bit[numRegisters];
+        for (int i = 0; i < numRegisters; i++) {
+            this.registers[i] = new Register_1Bit();
+        }
+        this.dmux = new DMUX_8Way_Gate();
+        this.mux = new MUX_8Way_Gate(bitSize);
+    }
+
+    public boolean dataOut() {
+        return dataOut;
+    }
+
+    public void dataIn(boolean dataIn) {
+        this.dataIn = dataIn;
+    }
+
+    public void write(boolean write) {
+        this.write = write;
+    }
+
+    public void address(boolean[] address) {
+        if (address.length != this.address.length) throw new IllegalArgumentException();
+        this.address = address;
+        wire();
+    }
+
+    private void wire() {
+        this.dmux.setGate(this.write, this.address);
+        boolean[][] muxInput = new boolean[numRegisters][bitSize];
+        for (int r = 0; r < numRegisters; r++) {
+            this.registers[r].write(this.dmux.getOutput()[r]);
+            this.registers[r].data(dataIn);
+            muxInput[r][0] = this.registers[r].Q();
+        }
+        this.mux.setGate(muxInput, this.address);
+        this.dataOut = this.mux.getOutput()[0];
+    }
+}
---
+++ tests/org/jonathanoliveira/complex_components/RAM_8x1Test.java
@@ -0,0 +1,87 @@
+package org.jonathanoliveira.complex_components;
+
+import org.jonathanoliveira.utilities.Converter;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RAM_8x1Test {
+
+    @Test (expected = IllegalArgumentException.class)
+    public void checkInvalidAddress() throws Exception {
+        RAM_8x1 ram = new RAM_8x1();
+        ram.write(true);
+        ram.dataIn(true);
+        boolean[] address = Converter.convertToBooleans(new int[]{0,0});
+        ram.address(address);
+    }
+
+    @Test
+    public void checkRam() throws Exception {
+        RAM_8x1 ram = new RAM_8x1();
+        boolean[] addr1 = Converter.convertToBooleans(new int[]{0,0,0});
+        boolean[] addr2 = Converter.convertToBooleans(new int[]{0,0,1});
+        boolean[] addr3 = Converter.convertToBooleans(new int[]{0,1,0});
+        boolean[] addr4 = Converter.convertToBooleans(new int[]{0,1,1});
+        boolean[] addr5 = Converter.convertToBooleans(new int[]{1,0,0});
+        boolean[] addr6 = Converter.convertToBooleans(new int[]{1,0,1});
+        boolean[] addr7 = Converter.convertToBooleans(new int[]{1,1,0});
+        boolean[] addr8 = Converter.convertToBooleans(new int[]{1,1,1});
+        ram.address(addr1);
+        assertTrue(!ram.dataOut());
+        ram.address(addr5);
+        assertTrue(!ram.dataOut());
+        ram.write(true);
+        ram.dataIn(true);
+        ram.address(addr1);
+        ram.address(addr3);
+        ram.address(addr5);
+        ram.address(addr7);
+        ram.write(false);
+        ram.address(addr1);
+        assertTrue(ram.dataOut());
+        ram.address(addr3);
+        assertTrue(ram.dataOut());
+        ram.address(addr5);
+        assertTrue(ram.dataOut());
+        ram.address(addr7);
+        assertTrue(ram.dataOut());
+        ram.address(addr2);
+        assertTrue(!ram.dataOut());
+        ram.address(addr4);
+        assertTrue(!ram.dataOut());
+        ram.address(addr6);
+        assertTrue(!ram.dataOut());
+        ram.address(addr8);
+        assertTrue(!ram.dataOut());
+        ram.write(true);
+        ram.dataIn(false);
+        ram.address(addr1);
+        ram.address(addr3);
+        ram.address(addr5);
+        ram.address(addr7);
+        ram.write(false);
+        ram.address(addr1);
+        assertTrue(!ram.dataOut());
+        ram.address(addr3);
+        assertTrue(!ram.dataOut());
+        ram.address(addr5);
+        assertTrue(!ram.dataOut());
+        ram.write(true);
+        ram.dataIn(true);
+        ram.address(addr7);
+        ram.write(false);
+        ram.address(addr5);
+        assertTrue(!ram.dataOut());
+        ram.address(addr2);
+        assertTrue(!ram.dataOut());
+        ram.address(addr4);
+        assertTrue(!ram.dataOut());
+        ram.address(addr6);
+        assertTrue(!ram.dataOut());
+        ram.address(addr8);
+        assertTrue(!ram.dataOut());
+        ram.address(addr7);
+        assertTrue(ram.dataOut());
+    }
+}
